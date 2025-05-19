07aada1352e2a179746b0ed1c97ca3d0a391aece
MoSam007 <samato.moma@gmail.com>
2025-05-19 08:46:05 +0300

    Updated env variables

--- src/index.tsx
+++ src/index.tsx
@@ -6,22 +6,10 @@ import App from './App';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 
-declare global {
-  interface ImportMetaEnv {
-    readonly VITE_CLERK_PUBLISHABLE_KEY?: string;
-    // add other env variables here if needed
-  }
-
-  interface ImportMeta {
-    readonly env: ImportMetaEnv;
-  }
-}
-
 const clerkPublishableKey = process.env.REACT_APP_CLERK_PUBLISHABLE_KEY;
 
-
 if (!clerkPublishableKey) {
-  throw new Error("Missing Clerk Publishable Key in .env (CLERK_PUBLISHABLE_KEY)");
+  throw new Error("Missing Clerk Publishable Key in .env (REACT_APP_CLERK_PUBLISHABLE_KEY)");
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
